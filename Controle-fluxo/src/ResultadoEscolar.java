5fd41f48bd6c3ca88fb36581e8dcd6ae7162f891
Gessyca Borges <gessyca.borges@gmail.com>
2024-06-03 22:31:35 -0300

    Estruturas condicionais

--- Controle-fluxo/src/ResultadoEscolar.java
+++ Controle-fluxo/src/ResultadoEscolar.java
@@ -12,5 +12,14 @@ public class ResultadoEscolar {
 
         else
             System.out.println("Reprovado");
+
+        // Cenário 1
+        String resultado1 = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println("Cenário 1 ternário: " + resultado1);
+
+
+        // Cenário 2
+        String resultado2 = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println("Cenário 2 ternário: " + resultado2);
     }
 }
